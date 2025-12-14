3457b4db74e4a97bde0bb9ff264886d0f434234b
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-12-14 18:09:25 -0400

    feat: Implement image loading in ImageViewHolder and enhance loadImages method in MainActivity

--- app/src/main/java/com/greetingsapp/mobile/adapter/ImageViewHolder.kt
+++ app/src/main/java/com/greetingsapp/mobile/adapter/ImageViewHolder.kt
@@ -1,16 +1,27 @@
 package com.greetingsapp.mobile.adapter
 
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.greetingsapp.mobile.R
 import com.greetingsapp.mobile.databinding.ItemImageBinding
 import com.greetingsapp.mobile.model.ImageModel
 
-// Esta clase interna "retiene" las vistas de una tarjeta para no buscarlas todo el tiempo
+// Esta clase interna "retiene" las vistas de una tarjeta para no buscarlas todo el tiempo gracias al viewBinding
 class ImageViewHolder(private val binding: ItemImageBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(image: ImageModel) {
-        // AQUÍ OCURRE LA MAGIA: Conectamos los datos con la vista
-        // Por ahora solo usaremos un placeholder, en la siguiente misión cargaremos la URL real
-        // binding.imageViewItem.load(image.imageUrl) <--- Esto lo haremos con Coil/Glide pronto
+
+        //URL temporal que representa nueva maquina usada mientras desarrollamos, ya que por defecto si usuramos
+        // localhost estariamos apuntando al emulador/dispositivo android no nuestra pc
+        val fixedUrl = image.imageUrl.replace("localhost", "10.0.2.2")
+
+        // 1. Cargar la imagen usando Coil
+        // La extensión .load viene de la librería Coil
+        binding.imageViewItem.load(fixedUrl) {
+            crossfade(true) // Efecto de desvanecimiento suave al aparecer
+            placeholder(R.drawable.ic_launcher_background) // Muestra esto mientras carga
+            error(R.drawable.ic_launcher_background) // Muestra esto si falla (ej. sin internet)
+        }
     }
 
 }
